fce532ecf16bd1766fe7223dd8f33ad21fc1aab0
CC91313 <50639267+CC91313@users.noreply.github.com>
2023-09-04 04:25:07 +0800

    更新 diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -30,7 +30,7 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 #
 # Add luci-app-amlogic
 #svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-svn co https://github.com/kenzok8/small/tree/master/luci-app-passwall small/luci-app-passwall
+svn co https://github.com/kenzok8/small/tree/master/luci-app-passwall package/luci-app-passwall
 # Fix runc version error
 # rm -rf ./feeds/packages/utils/runc/Makefile
 # svn export https://github.com/openwrt/packages/trunk/utils/runc/Makefile ./feeds/packages/utils/runc/Makefile
